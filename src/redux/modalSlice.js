ce698b563b25050051cfc56d3bf08e4872117a77
Aleksander Szczepaniak <lequit7@gmail.com>
2022-09-18 09:24:08 +0200

    Cleanup inside modalSlice.js

--- src/redux/modalSlice.js
+++ src/redux/modalSlice.js
@@ -3,14 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 export const modalSlice = createSlice({
     name: "modal",
     initialState: {
-        newContractIsOpen: false,
-        newSaleIsOpen: false,
-        newItemIsOpen: false,
         newWithdrawIsOpen: false,
         newReturnIsOpen: false,
-        editSaleIsOpen: false,
-        editItemIsOpen: false,
-        editContractIsOpen: false,
 
         clientModal: {
             id: 'client',
@@ -56,49 +50,12 @@ export const modalSlice = createSlice({
         setSaleModal: (state, action) => {
             state.saleModal = action.payload;
         },
-
-        toggleNewClientModal: (state, action) => {
-            state.newClientIsOpen = action.payload
-        },
-        toggleNewContractModal: (state, action) => {
-            state.newContractIsOpen = action.payload
-        },
-        toggleNewSaleModal: (state, action) => {
-            state.newSaleIsOpen = action.payload
-        },
-        toggleNewItemModal: (state, action) => {
-            state.newItemIsOpen = action.payload
-        },
         toggleNewWithdrawModal: (state, action) => {
             state.newWithdrawIsOpen = action.payload
         },
         toggleNewReturnModal: (state, action) => {
             state.newReturnIsOpen = action.payload
         },
-        toggleEditSaleModal: (state, action) => {
-            state.editSaleIsOpen = action.payload
-        },
-        toggleEditItemModal: (state, action) => {
-            state.editItemIsOpen = action.payload
-        },
-        toggleEditContractModal: (state, action) => {
-            state.editContractIsOpen = action.payload
-        },
-        toggleEditClientModal: (state, action) => {
-            state.editClientIsOpen = action.payload
-        },
-        closeAllModals: (state) => {
-            state.newClientIsOpen = false
-            state.newContractIsOpen = false
-            state.newSaleIsOpen = false
-            state.newItemIsOpen = false
-            state.newWithdrawIsOpen = false
-            state.newReturnIsOpen = false
-            state.editSaleIsOpen = false
-            state.editItemIsOpen = false
-            state.editContractIsOpen = false
-            state.editClientIsOpen = false
-        }
     }
 })
 
@@ -108,17 +65,8 @@ export const {
     setItemModal,
     setSaleModal,
 
-    toggleNewClientModal,
-    toggleNewContractModal,
-    toggleNewSaleModal,
-    closeAllModals,
-    toggleNewItemModal,
-    toggleEditItemModal,
-    toggleEditSaleModal,
     toggleNewWithdrawModal,
     toggleNewReturnModal,
-    toggleEditContractModal,
-    toggleEditClientModal
 } = modalSlice.actions
 
 export default modalSlice.reducer
